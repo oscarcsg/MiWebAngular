21bd161a158290a651dac7156b398312d2aa5358
oscarcsg <velagomezoscar@gmail.com>
2026-03-24 09:30:09 +0100

    Control de acceso a ciertas páginas si no está loggeado listo.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,17 @@ import { HomeComponent } from './layouts/public/home/home';
 import { AcercaDeComponent } from './layouts/public/about/about';
 import { LoginComponent } from './layouts/public/login/login';
 import { DashboardComponent } from './layouts/private/dashboard/dashboard';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
+  // PÚBLICAS
   { path: '', component: HomeComponent },
   { path: '#', component: HomeComponent },
   { path: 'acerca-de', component: AcercaDeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+
+  // PRIVADAS
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from './auth';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(Auth);
+  const router = inject(Router);
+
+  if (authService.isLogged()) {
+    return true;
+  } else {
+    router.navigate(['/login']);
+    return false; // Bloquea el acceso si no está loggeado
+  }
+};
